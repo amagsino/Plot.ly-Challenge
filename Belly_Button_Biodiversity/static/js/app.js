a7e37e385db7c06e0f9829cbdd5b4fc09205c459
Aizelmarie M <amagsino@users.noreply.github.com>
2020-02-21 16:01:37 -0500

    edit app

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -16,3 +16,79 @@ function optionChanged(selectedID){
           console.log(item.id);
           
        });
+    console.log(idMetadata);
+    const panelDisplay = d3.select("#sample-metadata");
+    panelDisplay.html("");
+    Object.entries(idMetadata[0]).forEach(item=> 
+       {
+          console.log(item);
+          panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
+       });
+ 
+    const idSample = data.samples.filter(item => parseInt(item.id) == selectedID);
+    
+    console.log(typeof parseInt(item.id));
+    console.log(idSample[0].sample_values);  
+    console.log(idSample[0].otu_ids);  
+    console.log(idSample[0].otu_labels);  
+    
+    var sampleValue = idSample[0].sample_values.slice(0,10);
+    sampleValue= sampleValue.reverse();
+    var otuID = idSample[0].otu_ids.slice(0,10);
+    otuID = otuID.reverse();
+    var otuLabels = idSample[0].otu_labels
+    otuLabels = otuLabels.reverse();
+ 
+    console.log(sampleValue);
+    console.log(otuID);
+    console.log(otuLabels);
+ 
+    const yAxis = otuID.map(item => 'OTU' + " " + item);
+       console.log(yAxis);
+    
+       const trace = {
+       y: yAxis,
+       x: sampleValue,
+       type: 'bar',
+       orientation: "h",
+       text:  otuLabels,
+       marker: {
+          color: 'rgb(154, 140, 152)',
+          line: {
+             width: 3
+         }
+        }
+       },
+       layout = {
+       title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
+       xaxis: {title: 'Number of Samples Collected'},
+       yaxis: {title: 'OTU ID'}
+       };
+ 
+       // Plotting the graph using plotly----------------------------------------
+       Plotly.newPlot('bar', [trace], layout,  {responsive: true});    
+       
+ var sampleValue1 =idSample[0].sample_values;
+ var otuID1= idSample[0].otu_ids;
+ 
+ const trace1 = {
+    x: otuID1,
+    y: sampleValue1,
+    mode: 'markers',
+    marker: {
+      color: otuID1,
+      
+      size: sampleValue1
+    }
+  },
+ 
+  layout1 = {
+    title: '<b>Bubble Chart For Each Sample</b>',
+    xaxis: {title: 'OTU ID'},
+    yaxis: {title: 'Number of Samples Collected'},
+    showlegend: false,
+    height: 800,
+    width: 1800
+    };
+    
+ Plotly.newPlot('bubble', [trace1], layout1);
